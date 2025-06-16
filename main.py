0b7de5883dcd05f1ec1788bbc23b0b1816ecca77
kinoco-yama <kinoyama001@gmail.com>
2025-06-17 03:58:09 +0900

    sample_wclを実装

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from aics_excel_loader import AicsExcelLoader
+from sample_wcl import SampleWCL
 
 
 def main():
@@ -10,15 +11,19 @@ def main():
     rssi = AicsExcelLoader(data_folda + "/measured_RSSI.xlsx")
 
     # データの確認
-    # print(ap.data)  # シートが1枚の時：keyは必要ない
-    # print(location.data["3"])  # シートが複数枚の時：keyはシート名
+    # print(ap.data)  # シートが1枚の時：データフレーム型
+    # print(location.data["3"])  # シートが複数枚の時：辞書型（keyはシート名）
     # print(location.data["4"])  # 処理しやすいよう、シート名を階数に変更している
-    # print(rssi.data["3"])
+    # print(rssi.data["3"])  # key指定したdata["3"]等は、データフレーム型
     # print(rssi.data["4"])
 
     # カラムの確認
     # print(location.data["3"].columns)  # .columnsでカラム名を確認できる
     # print(location.data["3"]["x"])
+
+    sampleWcl = SampleWCL(ap.data, rssi.data)
+    T = sampleWcl.calculate_coordinate(3, 15, 3)
+    print(f"推定位置座標 T: {T}")  # 推定位置座標 T を出力
 
 
 if __name__ == "__main__":
---
+++ sample_wcl.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+
+# テキスト内のWCLを実装
+class SampleWCL:
+    def __init__(self, ap, rssi):
+        self.ap = ap
+        self.rssi = rssi
+
+    def calculate_coordinate(self, floor, p, l):
+        """
+        座標の計算を行うメソッド
+
+        args:
+            floor: 階数
+            p: Location index P
+            l: アンカーノードの個数 L
+
+        return:
+            T: 推定位置座標 T
+        """
+
+        # 指定階数で計測したデータを抽出
+        self.rssi = self.rssi[str(floor)]
+
+        """
+        以下、テキストより抜粋
+        3階では3階に設置されたAPからのRSSIのみを、4階では4階に設置されたRSSIのみを位置推定に用いる。
+        """
+        # 1. 指定階数のAPを抽出
+        check_str = str(floor) + "F"
+        rssi_floor_only = self.rssi[self.rssi["AP_name"].str.contains(check_str)]
+        print(f"{floor}階のAP\n{rssi_floor_only}\n")  # デバッグ用
+
+        # 2. 位置PのRSSIデータを抽出
+        rssi_p = rssi_floor_only[rssi_floor_only["Location index P"] == p]
+        print(f"位置PのRSSIデータ\n{rssi_p}\n")  # デバッグ用
+
+        """
+        以下、テキストより抜粋
+        RSSIの中央値が大きい順に上位三つのAPを選択する。
+        """
+        # 3. 中央値の上位三つ(L個)を抽出
+        # 降順でソート
+        rssi_sorted = rssi_p.sort_values(by="MED (dBm)", ascending=False)
+        # 上位三つ(L個)のRSSI値を取得
+        rssi_med = rssi_sorted[0:l]
+        print(f"上位{l}個のRSSI値\n{rssi_med}\n")  # デバッグ用
+
+        """
+        以下、テキストより抜粋
+        重みは、上位三つの中でRSSIの中央値が最小のもの、つまり大きさ3番目の値との差[dB]を求め、その真値とする。
+        """
+        # 4. 重みを計算
+        weight = []
+        for rssi in rssi_med["MED (dBm)"]:
+            weight.append(float(rssi - rssi_med["MED (dBm)"].min()))
+        print(f"重み\n{weight}\n")  # デバッグ用
+
+        # 5. 座標を取得
+        coordinate = []
+        for rssi_name in rssi_med["AP_name"]:
+            coordinate.append(
+                (
+                    float(self.ap[self.ap["AP_name"] == rssi_name]["x"].iloc[0]),
+                    float(self.ap[self.ap["AP_name"] == rssi_name]["y"].iloc[0]),
+                )
+            )
+        print(f"座標\n{coordinate}\n")  # デバッグ用
+
+        # 6. 座標の計算
+        x = sum(w * coord[0] for w, coord in zip(weight, coordinate)) / sum(weight)
+        y = sum(w * coord[1] for w, coord in zip(weight, coordinate)) / sum(weight)
+        T = (x, y)
+
+        return T
