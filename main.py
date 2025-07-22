b595c9693c2792f136df96c53950434a086adb12
kohama-yujin <kohama.yujin.mf@tut.jp>
2025-07-23 02:49:51 +0900

    samplewclの計算値が合わない

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from wcl import WCL
 from overlap_ap import Overlapap
 from count import Count
 from frequency_filter import FrequencyFilter
+import traceback
 import csv
+import math
+import numpy as np
+from itertools import zip_longest
 
 
 def main():
@@ -42,6 +46,10 @@ def main():
     all_results = []
     method_results = []
     floor_results = []
+    # 実測値リスト
+    all_correct = []
+    method_correct = []
+    floor_correct = []
     # 手法リスト
     methods = ["SampleWCL", "5GHz", "24GHz"]
     # 階数リスト
@@ -49,26 +57,49 @@ def main():
 
     for method in methods:
         method_results = []  # 初期化
+        method_correct = []
         for floor in floors:
             floor_results = []  # 初期化
+            floor_correct = []
             for p in range(1, 60):
                 try:
-                    weight, coordinate = sampleWcl.get_weight_and_coords(floor, p, 3)
+                    if method == "SampleWCL":
+                        weight, coordinate = sampleWcl.get_weight_and_coords(
+                            floor, p, 3
+                        )
+                    elif method == "5GHz":
+                        weight, coordinate = (
+                            filtered_5ghz_sampleWcl.get_weight_and_coords(floor, p, 3)
+                        )
+                    elif method == "24GHz":
+                        weight, coordinate = (
+                            filtered_24ghz_sampleWcl.get_weight_and_coords(floor, p, 3)
+                        )
                     # 推定位置座標 T を計算
                     wcl = WCL(weight, coordinate)
                     T = wcl.calculate_coordinate()
+                    print(T)
+                    rounded_T = tuple(round(x, 3) for x in T)
                     # 推定結果を階数結果リストに追加
                     floor_results.append(
                         {
                             "position": p,
-                            "estimated_coordinate": T,
+                            "estimated_coordinate": rounded_T,
                         }
+                    )
+                    # 実測値
+                    floor_correct.append(
+                        (
+                            int(location.data[str(floor)]["x"][p - 1]),
+                            int(location.data[str(floor)]["y"][p - 1]),
+                        )
                     )
                 # 例外処理を追加
                 except Exception as e:
                     print(
                         f"\033[33mSampleWCL 位置P={p}: エラーが発生しました - {e}\033[0m"
                     )
+                    traceback.print_exc()
                     continue
             # 階数ごとの結果を手法結果リストに追加
             method_results.append(
@@ -77,41 +108,124 @@ def main():
                     "results": floor_results,
                 }
             )
+            method_correct.append(floor_correct)
             print(
                 f"{floor}階での処理完了: {len(floor_results)}個の位置で推定が成功しました"
             )
         # 手法ごとの推定結果をすべての結果リストに追加
         all_results.append({"name": method, "results": method_results})
+        all_correct.append(method_correct)
         print(f"\033[32m{method}の推定が終わりました\033[0m")
 
-    for method in all_results:
-        # CSVファイルに結果を出力
+    # CSVファイルに結果を出力
+    for method_index, method in enumerate(all_results):
         csv_filename = f'results/{method["name"]}.csv'
         # ファイルオープン
         with open(csv_filename, "w", newline="", encoding="utf-8") as csvfile:
             writer = csv.writer(csvfile)
+            # タイトルを作成
+            title = []
+            for floor in method["results"]:
+                title.extend(
+                    [
+                        f"{floor['floor']}F-{method['name']}",
+                        "estimated",
+                        "",
+                        "correct",
+                        "",
+                        "",
+                    ]
+                )
+            # タイトルを書き込み
+            writer.writerow(title)
             # ヘッダーを作成
             header = []
             for floor in method["results"]:
                 header.extend(
                     [
-                        f"{floor['floor']}F-{method['name']}",
-                        "X_Coordinate",
-                        "Y_Coordinate",
+                        "",
+                        "x",
+                        "y",
+                        "x",
+                        "y",
+                        "error",
                     ]
                 )
             # ヘッダーを書き込み
             writer.writerow(header)
+
             # データを作成
-            data = []
-            for floor in method["results"]:
-                for results in floor["results"]:
-                    position = results["position"]
-                    x_coord = results["estimated_coordinate"][0]
-                    y_coord = results["estimated_coordinate"][1]
-                    data.extend([position, x_coord, y_coord])
-            # データを書き込み
-            writer.writerow(data)
+            total_error = []
+            for positions_index, positions in enumerate(
+                zip_longest(*[floor["results"] for floor in method["results"]]), start=1
+            ):
+                row = []
+                position_error = []
+                for floor_index, floor_result in enumerate(positions):
+                    if floor_result is None:
+                        row += ["", "", "", "", "", ""]
+                    else:
+                        position = floor_result["position"]
+                        estimated_coordinate = list(
+                            floor_result["estimated_coordinate"]
+                        )
+                        correct_coordinate = list(
+                            all_correct[method_index][floor_index][positions_index - 1]
+                        )
+                        # 誤差計算
+                        error = math.sqrt(
+                            pow(
+                                estimated_coordinate[0] - correct_coordinate[0],
+                                2,
+                            )
+                            + pow(
+                                estimated_coordinate[1] - correct_coordinate[1],
+                                2,
+                            )
+                        )
+                        row += (
+                            [position]
+                            + estimated_coordinate
+                            + correct_coordinate
+                            + [round(error, 3)]
+                        )
+                        position_error.append(error)
+                total_error.append(position_error)
+                # データを書き込み
+                writer.writerow(row)
+            # フッターの計算
+            total_error = list(map(list, zip_longest(*total_error)))
+            average_error = []  # 平均
+            max_error = []  # 最大値
+            min_error = []  # 最小値
+            std_population = []  # 標準偏差
+            variance_population = []  # 分散
+            for error in total_error:
+                # Noneを除外
+                error_not_none = [x for x in error if x is not None]
+                average_error.append(round(np.mean(error_not_none), 3))
+                max_error.append(round(np.max(error_not_none), 3))
+                min_error.append(round(np.min(error_not_none), 3))
+                std_population.append(round(np.std(error_not_none), 3))
+                variance_population.append(round(np.var(error_not_none), 3))
+            # フッターを作成
+            footer_ave = []
+            footer_max = []
+            footer_min = []
+            footer_std = []
+            footer_var = []
+            for i, floor in enumerate(method["results"]):
+                footer_ave.extend(["", "", "", "", "Ave", average_error[i]])
+                footer_max.extend(["", "", "", "", "Max", max_error[i]])
+                footer_min.extend(["", "", "", "", "Min", min_error[i]])
+                footer_std.extend(["", "", "", "", "Std", std_population[i]])
+                footer_var.extend(["", "", "", "", "Var", variance_population[i]])
+            # フッターを書き込み
+            writer.writerow(footer_ave)
+            writer.writerow(footer_max)
+            writer.writerow(footer_min)
+            writer.writerow(footer_std)
+            writer.writerow(footer_var)
         print(f"\033[32m結果をCSVファイル '{csv_filename}' に保存しました\033[0m")
 
 
